1278f984001cb85a240ba31f3b6ab3fca6f74b9e
hennign <hennign@onid.oregonstate.edu>
2016-11-23 23:32:33 -0800

    Adds button that calls function from app.component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 @Component({
+  // Using systemjs sets module.id to the root directory
+  // so to use templateUrl, must use absolute path
+  // https://github.com/angular/angular/issues/6131
+  // moduleId: module.id,
   selector: 'my-app',
-  template: '<h1>Hello Angular!</h1>'
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+    destroyMoon(): void {
+        window.alert("You destroyed the Moon! You monster...");
+      }
+
+}
