862fe70938cd3f76551f81108bc02df2eefc2410
hennign <hennign@onid.oregonstate.edu>
2016-11-29 01:32:52 -0800

    Completes Tour of Heroes part 5. Adds favicon.

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
-import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+import { AppRoutingModule }     from './app-routing.module';
+
+// Note: I had a problem with the <base href="/"> in index.html
+// not being recognized. I finally fixed it by updating my config
+// files, although I'm not certain exactly what was generating the
+// issue. The commented out code referencing APP_BASE_HREF was a workaround.
+//import { APP_BASE_HREF }        from '@angular/common';
+
 
 @NgModule({
-  imports:      [ 
-      BrowserModule,
-      FormsModule
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  declarations: [ 
-      AppComponent,
-      HeroDetailComponent 
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [ 
+    HeroService,
+    //{provide: APP_BASE_HREF, useValue : '/' }
+  ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
